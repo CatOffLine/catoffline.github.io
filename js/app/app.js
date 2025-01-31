49149361e6e8175229c3da7ca4c8747e0519eeb6
Maksym Chychkan <LineOnCat@protonmail.com>
2025-01-31 17:44:46 +0200

    minor fixes

--- js/app/app.js
+++ js/app/app.js
@@ -6,12 +6,12 @@ import {msg} from './widgets/msg.js';
 import {toogle} from './widgets/toogle.js';
 import {img} from './widgets/img.js';
 
-document.addEventListener(;DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function(){
 	const main = {
 		data(){
 		return {
 			url:"https://affiliate.chychkan.com",
-			user:{name:"", phone"", email:"", date:"", auth:"",
+			user:{name:"", phone:"", email:"", date:"", auth:""},
 			formData:{},
 			title:"",
 			date:"",
@@ -25,59 +25,62 @@ document.addEventListener(;DOMContentLoaded', function(){
 		},
 		mounted:function(){
 				this.init();
-			}
-		},	
-methods:(){
-var self = this;
-if(window.localStorage.getItem('user')){
-self.user = JSON.parse(window.localStorage.getItem("user"));
-if(self.$route['path']=='/' && self.user.type=='admin'){
-self page('/campaigns');
-}else if(['/campaigns','/campaign','/users','/user'].includes(self.$route['path']) && self.user.type!='admin'){	
-self page('/statitics');
-}else if(['/statitics','/payments','/sites'].includes(self.$route['path']) && self.user.type!='admin'){
-	self page('campaigns');
-}else if(['campaigns','campaign','/users','user','/statitics','/payments','/sites'].includes(self.$route['path']) && self.user.type!='admin'){
-	self page('');	
-}else if(['campaigns','campaign','/users','user','/statitics','/payments','/sites'].includes(self.$route['path']) && self.user.type!='admin'){
-	self page('');
-}
-}else{
-	self.page('/')
-}
-	}};
-},
-logout(){
-	this.user = {name:"", phone"", email:"", date:"", auth:""};
-	this.page('/');
-	window.localStorage.setItem('user','');
-},
+			},	
+		methods:{
+			init(){
+				var self = this;
+				if(window.localStorage.getItem('user')) self.user = JSON.parse(window.localStorage.getItem("user"));
 
-pagefunction (path=""){
-this.$router.replace(path);
-this.title=this.$route['name'];
-document.title=this.$route['name'];
-},
-toFormData:function(obj){
-	var fd = new FormData();
+				router.isReady().then(() => {
+					if(window.localStorage.getItem('user')){
+				self.user = JSON.parse(window.localStorage.getItem("user"));
+				if(self.$route['path']=='/' && self.user.type=='admin'){
+					self.page('/campaigns');
+				}else if(['/campaigns','/campaign','/users','/user'].includes(self.$route['path']) && self.user.type!='admin'){	
+					self.page('/statitics');
+				}else if(['/statitics','/payments','/sites'].includes(self.$route['path']) && self.user.type!='admin'){
+					self.page('campaigns');
+				}else if(['campaigns','campaign','/users','user','/statitics','/payments','/sites'].includes(self.$route['path']) && self.user.type!='admin'){
+					self.page('');	
+				}else if(['campaigns','campaign','/users','user','/statitics','/payments','/sites'].includes(self.$route['path']) && self.user.type!='admin'){
+					self.page('');
+				} 
+			}else{
+				self.page('/')
+			}
+		});
+	},
+	logout(){
+		this.user = {name:"", phone:"", email:"", date:"", auth:""};
+		this.page('/');
+		window.localStorage.setItem('user','');
+	},
+
+	pagefunction (path=""){
+		this.$router.replace(path);
+		this.title=this.$route['name'];
+		document.title=this.$route['name'];
+	},
+	toFormData:function(obj){
+		var fd = new FormData();
 	
-	for(var x in obj){
-	if(typeof obj[x]==='object' && x != 'img' && != 'copy'){
-	for(var y in obj[x]}{
-		if(typeof obj[x==='object']){
-		for(var z in obj[x][y]){
-			fd.append(x+'['+y+']['+z+']', obj[x][y][z]);
+		for(var x in obj){
+			if(typeof obj[x]==='object' && x != 'img' && x != 'copy'){
+				for(var y in obj[x]){
+					if(typeof obj[x==='object']){
+						for(var z in obj[x][y]){
+							fd.append(x+'['+y+']['+z+']', obj[x][y][z]);
+						}
+					}else{
+						fd.append(x+'['+y+']', obj [x][y]);
+					}
+				}
+			}else if(x != 'copy'){
+				fd.append(x, obj[x]);
+			}
 		}
-		}else{
-			fd.append(x+'['+y+']', obj [x][y]);
+		return fd;
 		}
-	}
-}else if(x != 'copy'){
-	fd.append(x, obj[x]);
-}
-}
-return fd;
-}
 }
 };
 
